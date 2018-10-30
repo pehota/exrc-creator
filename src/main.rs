4e4a83114ef064419936356054cfe957439f0363
Dimitar Apostolov <>
2018-10-30 22:16:05 +0100

    Parse languages parameter

--- src/main.rs
+++ src/main.rs
@@ -44,25 +44,17 @@ fn get_args() -> Args {
     let mut arg_name: String = "".to_string();
 
     for arg in args.iter().skip(1) {
-        println!("current arg: {}", arg);
         if arg[..1] == "-".to_string() && arg_name.is_empty() {
             arg_name = arg[1..arg.len()].to_string();
         } else if !arg_name.is_empty() {
             if arg_name == "dst".to_string() {
                 arguments.dst_dir = arg.to_string();
             } else if arg_name == "languages".to_string() {
-                // arguments.languages = arg
-                // .to_string()
-                // .split(',')
-                // .collect::<Vec<&str>>()
-                // .into_iter()
-                // .map(|str| str.to_string())
-                // .to_vec();
-
-                println!(
-                    "split: {:?}",
-                    arg.to_string().split(',').collect::<Vec<&str>>()
-                );
+                arguments.languages = arg
+                    .to_string()
+                    .split(',')
+                    .map(String::from)
+                    .collect::<Vec<String>>();
             }
             arg_name = "".to_string();
         }
