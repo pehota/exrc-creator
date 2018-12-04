433fe68b9c877bd74e9bd02de7fc9f19fcccaf9e
Dimitar Apostolov <>
2018-12-04 23:33:42 +0100

    Print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     use std::env;
 
     let config = Config::new(env::args().collect()).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("{}", err);
         process::exit(1);
     });
 
     if let Err(error) = run(config) {
-        println!("An error occurred while running the app: {}", error);
+        eprintln!("{}", error);
         process::exit(1);
     }
 }
